f83b5792a43ff05da5967f30f6412e6aaceb35c0
Harry Clark <107435091+hazzaclark@users.noreply.github.com>
2025-06-03 00:22:35 +0100

    added instances to be able to read and write

--- Memory/ProcessMemory.cs
+++ Memory/ProcessMemory.cs
@@ -5,24 +5,42 @@
 // IN TURN, THIS WILL ALLOW FOR FULL MODULAR CONTROL IN HOOKING MEMORY MORE EFFICIENTLY
 // BASED ON EVENTS
 
-﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Diagnostics;
-using System.Runtime.InteropServices;
-using System.Text;
-using System.Linq;
-
-using revoMem.Memory.Structs;
+using revoMem.Memory.Interfaces;
+using System.Runtime.CompilerServices;
 
 namespace revoMem.Memory;
 
-public class ProcessMemory : IDisposable
+public unsafe partial struct ProcessMemory : MemoryAccess
 {
-    public ProcessMemory() {}
+    public static readonly ProcessMemory INSTANCE = new();
 
-    public void Dispose()
+    /// <summary>
+    /// READS RAW BYTES FROM A SPECIFIED MEMORY OFFSET
+    /// WRITES THOSE CONTENTS INTO THE PROVIDED SPAN WHICH ENCOMPASSING AN ARBITRARY MEMORY REGION
+    /// </summary>
+    /// <param name="OFFSET">THE MEMORY ADDRESS TO READ FROM</param>
+    /// <param name="VALUE">THE DESTINATION SPAN WHERE THE READ BYTES WILL BE WRITTEN TO</param>
+    /// <remarks>
+    /// THE METHOD IN QUESTION PERFORMS AN UNALIGNED MEMORY COPY WHICH ASSUMES THAT THE SOURCE MEMORY REGION
+    /// IS ASSOCIATED WITH THE CURRENT OFFSET, AS WELL AS IT BEING VALID TO BEGIN WITH
+    /// </remarks>
+    public void READ_RAW(uint OFFSET, Span<byte> VALUE)
     {
+        fixed (byte* RESULT = VALUE)
+        {
+            Unsafe.CopyBlockUnaligned(RESULT, (void*)OFFSET, (uint)VALUE.Length);
+        }
+    }
 
+    /// <summary>
+    /// DO THE SAME UNALIGNED MEMORY SCHEME BUT FOR WRITES 
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
+    /// <param name="OFFSET"></param>
+    /// <param name="BASE"></param>
+
+    public void WRITE<T>(uint OFFSET, T BASE)
+    {
+        Unsafe.WriteUnaligned((void*)OFFSET, BASE);
     }
 }
